a51d9d26decdc5039a0ef949ff5a19c354138e77
onesine <philemonlewhe@gmail.com>
2024-08-10 10:17:55 +0100

    [Bubble Sort] Change sorte index color ✅

--- src/page/bubble-sort.tsx
+++ src/page/bubble-sort.tsx
@@ -11,6 +11,12 @@ interface ArrayNumberType {
     value: number;
 }
 
+interface StepType {
+    swap: boolean;
+    swapIndex: number[];
+    maxStep: boolean;
+}
+
 const INITIAL_NUMBERS = [
     { id: "1", value: 7 },
     { id: "2", value: 61 },
@@ -39,6 +45,7 @@ const BubbleSort = () => {
     );
     const [array, setArray] = useState<ArrayNumberType[]>(INITIAL_NUMBERS);
     const [currentAnimate, setCurrentAnimate] = useState<number[]>([]);
+    const [sortIndex, setSortIndex] = useState<number[]>([]);
 
     const maxValue = useMemo(() => {
         return Math.max(...array.map((item) => item.value));
@@ -53,17 +60,30 @@ const BubbleSort = () => {
 
     const handleSort = useCallback(() => {
         const newArray = [...array.map((item) => ({ ...item }))];
-        const steps: number[][] = [];
+        const steps: StepType[] = [];
 
         for (let i = 0; i < newArray.length - 1; i++) {
             let swap = false;
             for (let y = 0; y < newArray.length - i - 1; y++) {
                 const temp = newArray[y].value;
+                const step = [y, y + 1];
+                const maxStep = y === newArray.length - i - 2;
+
                 if (temp > newArray[y + 1].value) {
                     newArray[y].value = newArray[y + 1].value;
                     newArray[y + 1].value = temp;
-                    steps.push([y, y + 1]);
+                    steps.push({
+                        swap: true,
+                        swapIndex: step,
+                        maxStep: maxStep
+                    });
                     swap = true;
+                } else {
+                    steps.push({
+                        swap: false,
+                        swapIndex: step,
+                        maxStep: maxStep
+                    });
                 }
             }
 
@@ -73,11 +93,18 @@ const BubbleSort = () => {
         setArrayStep("sorting");
         steps.forEach((step, index) => {
             setTimeout(() => {
-                const [a, b] = step;
+                const {
+                    swap,
+                    swapIndex: [a, b],
+                    maxStep
+                } = step;
+
                 setCurrentAnimate([a, b]);
                 setArray((prevArray) => {
                     const newItems = [...prevArray];
-                    [newItems[a], newItems[b]] = [newItems[b], newItems[a]];
+                    if (swap) {
+                        [newItems[a], newItems[b]] = [newItems[b], newItems[a]];
+                    }
 
                     return newItems;
                 });
@@ -85,6 +112,20 @@ const BubbleSort = () => {
                 if (index === steps.length - 1) {
                     setArrayStep("sort");
                     setCurrentAnimate([]);
+                }
+
+                if (maxStep) {
+                    setSortIndex((prevState) => {
+                        const lastsIndex: number[] = [];
+
+                        if (index === steps.length - 1 && b > 0) {
+                            for (let i = b - 1; i >= 0; i--) {
+                                lastsIndex.push(i);
+                            }
+                        }
+
+                        return [...prevState, b, ...lastsIndex];
+                    });
                 }
             }, index * 1000);
         });
@@ -105,6 +146,7 @@ const BubbleSort = () => {
                             : () => {
                                   setArray(INITIAL_NUMBERS);
                                   setArrayStep("initial");
+                                  setSortIndex([]);
                               }
                     }
                     disabled={arrayStep === "sorting"}
@@ -127,16 +169,18 @@ const BubbleSort = () => {
                                 }}
                                 layout
                                 animate={{
-                                    backgroundColor: currentAnimate.includes(
+                                    backgroundColor: sortIndex.includes(
                                         itemIndex
                                     )
-                                        ? "#fbbf24"
-                                        : "#cbd5e1",
-                                    borderColor: currentAnimate.includes(
-                                        itemIndex
-                                    )
-                                        ? "#fbbf24"
-                                        : "#94a3b8"
+                                        ? "#84cc16"
+                                        : currentAnimate.includes(itemIndex)
+                                          ? "#fbbf24"
+                                          : "#cbd5e1",
+                                    borderColor: sortIndex.includes(itemIndex)
+                                        ? "#84cc16"
+                                        : currentAnimate.includes(itemIndex)
+                                          ? "#fbbf24"
+                                          : "#94a3b8"
                                 }}
                                 transition={{
                                     layout: {
